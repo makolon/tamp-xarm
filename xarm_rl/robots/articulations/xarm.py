7eee9e6383ade6b4f68dc0077bd6126a98a74e26
makolon <s.makolon@icloud.com>
2024-02-08 00:17:17 +0900

    [WIP] fix stiffness and damping parameters

--- xarm_rl/robots/articulations/xarm.py
+++ xarm_rl/robots/articulations/xarm.py
@@ -37,6 +37,40 @@ class xArm(Robot):
             articulation_controller=None,
         )
 
+        dof_paths = [
+            "link_base/joint1",
+            "link1/joint2",
+            "link2/joint3",
+            "link3/joint4",
+            "link4/joint5",
+            "link5/joint6",
+            "link6/joint7",
+            "xarm_gripper_base_link/left_drive_joint",
+            "xarm_gripper_base_link/right_drive_joint",
+        ]
+
+        drive_type = ["angular"] * 9
+        default_dof_pos = [math.degrees(x) for x in [0.0, -0.698, 0.0, 0.349, 0.0, 1.047, 0.0, 0.0, 0.0]]
+        stiffness = [400 * np.pi / 180] * 7 + [80 * np.pi / 180] * 2
+        damping = [80 * np.pi / 180] * 7 + [160 * np.pi / 180] * 2
+        max_force = [87, 87, 87, 87, 12, 12, 12, 200, 200]
+        max_velocity = [math.degrees(x) for x in [2.175, 2.175, 2.175, 2.175, 2.61, 2.61, 2.61, 2.0, 2.0]]
+
+        for i, dof in enumerate(dof_paths):
+            set_drive(
+                prim_path=f"{self.prim_path}/{dof}",
+                drive_type=drive_type[i],
+                target_type="position",
+                target_value=default_dof_pos[i],
+                stiffness=stiffness[i],
+                damping=damping[i],
+                max_force=max_force[i],
+            )
+
+            PhysxSchema.PhysxJointAPI(get_prim_at_path(f"{self.prim_path}/{dof}")).CreateMaxJointVelocityAttr().Set(
+                max_velocity[i]
+            )
+
     def set_xarm_properties(self, stage, prim):
         for link_prim in prim.GetChildren():
             if link_prim.HasAPI(PhysxSchema.PhysxRigidBodyAPI): 
