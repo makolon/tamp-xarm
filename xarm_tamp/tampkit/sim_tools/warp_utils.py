cd401c3b7c7c0dab9a4722bb71ad399978dc7071
makolon <smakolon385@gmail.com>
2024-05-06 21:38:05 +0900

    fix warp utils

--- xarm_tamp/tampkit/sim_tools/warp_utils.py
+++ xarm_tamp/tampkit/sim_tools/warp_utils.py
@@ -1,10 +1,11 @@
 import os
 import pysdf
 import trimesh
-import numpy as NotImplementedError
+import numpy as np
 import warp as wp
 from pxr import Gf, Sdf, Usd, UsdGeom, UsdPhysics
 
+# Initialize warp
 wp.init()
 
 
@@ -18,31 +19,3 @@ def load_mesh(prim: Usd.Prim):
     )
 
     return mesh
-
-def get_distance(body1, body2):
-    path1 = body1.prim.GetPath()
-    path2 = body2.prim.GetPath()
-
-    if '.urdf' in path1:
-        urdf1 = pysdf.URDF.load(path1)
-        mesh1 = urdf.links[0].collision_mesh
-    else:
-        mesh1 = trimesh.load_mesh(path1)
-    if '.urdf' in path2:
-        urdf2 = pysdf.URDF.load(path2)
-        mesh2 = urdf.links[1].collision_mesh
-    else:
-        mesh2 = trimesh.load_mesh(path2)
-
-    # Get transform
-    transform1 = get_pose(body1)
-    transform2 = get_pose(body2)
-
-    # Transform mesh
-    mesh1.apply_transform(transform1)
-    mesh2.apply_transform(transform2)
-
-    distance = trimesh.proximity.sighed_distance(mesh1, mesh2.vertices)
-    min_distance = np.min(np.abs(distance))
-
-    return min_distance
