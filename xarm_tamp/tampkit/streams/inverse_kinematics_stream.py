3727298700c6593cbdb000e545ccf34f0e659e7d
makolon <smakolon385@gmail.com>
2024-05-15 21:35:40 +0900

    [WIP] fix streams

--- xarm_tamp/tampkit/streams/inverse_kinematics_stream.py
+++ xarm_tamp/tampkit/streams/inverse_kinematics_stream.py
@@ -9,62 +9,7 @@ from curobo.types.math import Pose
 from curobo.types.state import JointState
 
 
-def get_free_motion_fn(problem, collisions=True, teleport=False):
-    robot = problem.robot
-    tensor_args = problem.tensor_args
-    plan_cfg = problem.plan_cfg
-    ik_solver = problem.ik_solver
-    motion_planner = problem.motion_planner
-    obstacles = problem.fixed if collisions else []
-
-    def fn(arm, body, pose, grasp):
-        arm_joints = get_arm_joints(robot)
-
-        # Default confs
-        default_arm_conf = get_initial_conf(robot)
-
-        # Set position to default configuration for grasp action
-        assert len(default_arm_conf) == len(arm_joints), "Lengths do not match."
-
-        # parse pose
-        position, rotation = pose
-
-        # Set ik goal
-        ik_goal = Pose(
-            position=tensor_args.to_device(position),
-            quaternion=tensor_args.to_device(rotation),
-        )
-        goal_conf = ik_solver.solve_single(ik_goal)
-
-        # Get joint states
-        sim_js = robot.get_joints_state()
-
-        # Plan joint motion for grasp
-        curr_js = JointState(
-            position=tensor_args.to_device(sim_js.positions),
-            velocity=tensor_args.to_device(sim_js.velocities) * 0.0,
-            acceleration=tensor_args.to_device(sim_js.velocities) * 0.0,
-            jerk=tensor_args.to_device(sim_js.velocities) * 0.0,
-            joint_names=robot._arm_dof_names
-        )
-        curr_js = curr_js.get_ordered_joint_state(motion_planner.kinematics.joint_names)
-        result = motion_planner.plan_single(curr_js.unsqueeze(0), ik_goal, plan_cfg.clone())
-        succ = result.success.item()
-        if succ:
-            trajectory = result.get_interpolated_plan()
-        else:
-            carb.log_warn("Plan did not converge to a solution.")
-            return None
-
-        goal_conf = goal_conf.js_solution.position
-        conf = BodyConf(robot=robot, configuration=goal_conf)
-        command = Command([BodyPath(robot, trajectory)])
-        return (conf, command)
-
-    return fn
-
-
-def get_holding_motion_fn(problem, collisions=True, teleport=False):
+def get_ik_fn(problem, collisions=True):
     robot = problem.robot
     tensor_args = problem.tensor_args
     plan_cfg = problem.plan_cfg
